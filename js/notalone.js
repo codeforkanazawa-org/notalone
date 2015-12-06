72e89d954e54814ef614e3fb8cdf3ff5d534f5b4
cube11_admin <tada.10kio@gmail.com>
2015-12-06 20:39:10 +0900

    date picker 日本語化オプション設定

--- js/notalone.js
+++ js/notalone.js
@@ -168,13 +168,36 @@ $(function() {
 			//showButtonPanel: true,
 			changeMonth: true,
 			changeYear: true,
-			dateFormat:'yy/mm/dd'
+			dateFormat:'yy/MM/dd'
 		});
 
 		$(idname).datepicker("option", "showOn", 'button');
 		$(idname).datepicker("option", "buttonImageOnly", true);
 		$(idname).datepicker("option", "buttonImage", 'images/ico_calendar.png');
 	}
+});
+
+//datepicker　日本語化オプション
+$(function($){
+    $.datepicker.regional['ja'] = {
+        closeText: '閉じる',
+        prevText: '<前',
+        nextText: '次>',
+        currentText: '今日',
+        monthNames: ['01','02','03','04','05','06',
+        '07','08','09','10','11','12'],
+        monthNamesShort: ['1月','2月','3月','4月','5月','6月',
+        '7月','8月','9月','10月','11月','12月'],
+        dayNames: ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+        dayNamesShort: ['日','月','火','水','木','金','土'],
+        dayNamesMin: ['日','月','火','水','木','金','土'],
+        weekHeader: '週',
+        dateFormat: 'yy/mm/dd',
+        firstDay: 0,
+        isRTL: false,
+        showMonthAfterYear: true,
+        yearSuffix: '年'};
+    $.datepicker.setDefaults($.datepicker.regional['ja']);
 });
 
 
